da41a81fa137c607ca62a921baf300c09afa1e07
Euiseon Hong <hon3538@gmail.com>
2024-05-08 16:20:06 +0900

    web HTML, CSS

---
+++ Web/code/web_code/test/test.js
@@ -0,0 +1,3 @@
+console.log('hello kt aivle!');
+console.log(1);
+console.log(2);
---
+++ Web/code/web_code/test/test.py
@@ -0,0 +1,9 @@
+print('hello kt aivle!')
+
+
+def echo(msg):
+    print(msg)
+
+# ctrl + k , ctrl + s
+# shift + alt + j
+# 1 2 3
